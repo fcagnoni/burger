4c8f005d78cc294a68924b342d3032d1d971eb96
Francisco Cagnoni <fcagnoni@Franciscos-MacBook-Pro.local>
2019-08-24 19:37:02 -0600

    db working?

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');
+const fs = require('fs');
+const path = require('path');
 
 let connection;
 if (process.env.JAWSDB_URL) {
@@ -11,5 +13,15 @@ if (process.env.JAWSDB_URL) {
     database: 'burger_db'
   });
 }
+
+
+// initialize table and seed values
+var schemaSql = fs.readFileSync(path.join(__dirname, '..', 'db', 'schema.sql'), 'utf8');
+var seedsSql = fs.readFileSync(path.join(__dirname, '..', 'db', 'seeds.sql'), 'utf8');
+connection.query(schemaSql, (err, result) => {
+  connection.query(seedsSql, (err, result) => {
+    
+  });
+});
 
 module.exports = connection;
