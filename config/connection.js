00ab9e12cb8bd317b5d969ffd6fb8cae8e6e26bd
Francisco Cagnoni <fcagnoni@Franciscos-MacBook-Pro.local>
2019-08-24 19:46:04 -0600

    db works

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,4 @@
 const mysql = require('mysql');
-const fs = require('fs');
-const path = require('path');
 
 let connection;
 if (process.env.JAWSDB_URL) {
@@ -13,15 +11,5 @@ if (process.env.JAWSDB_URL) {
     database: 'burger_db'
   });
 }
-
-
-// initialize table and seed values
-var schemaSql = fs.readFileSync(path.join(__dirname, '..', 'db', 'schema.sql'), 'utf8');
-var seedsSql = fs.readFileSync(path.join(__dirname, '..', 'db', 'seeds.sql'), 'utf8');
-connection.query(schemaSql, (err, result) => {
-  connection.query(seedsSql, (err, result) => {
-    
-  });
-});
 
 module.exports = connection;
--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@
 
 * create a new burger to devour or pick an existing one.
 
-* built with MySQL, handlebars, custom ORM, deployed on Heroku
+* built with MySQL, handlebars, custom ORM, deployed on Heroku https://fc-burger-time.herokuapp.com/
